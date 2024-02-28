ba4cfa5da33337661197541789c3214e772098e6
rupika14 <85879889+rupika14@users.noreply.github.com>
2024-02-28 18:23:48 +0530

    Update check-if-a-number-is-divisible-by-8.py

--- Easy/Check if a number is divisible by 8/check-if-a-number-is-divisible-by-8.py
+++ Easy/Check if a number is divisible by 8/check-if-a-number-is-divisible-by-8.py
@@ -3,23 +3,12 @@
 class Solution:
     def DivisibleByEight(self,s):
         #code here
-        if s[-3:]=="000":
+       
+        n = int(s[-3:]) if len(s) >= 3 else int(s)
+        if n%8 == 0:
             return 1
-        elif len(s)>3:
-            n=int(s[-3:])
-            if n%8==0:
-                return 1
-            else: 
-                return -1 
-        elif len(s)<3:
-            n=int(s[-3:])
-            if n%8==0:
-                return 1
-            else: 
-                return -1
-        else: 
+        else:
             return -1
-
 
 #{ 
  # Driver Code Starts
