e7dd47251ff6b4c54d6da0d7421a0ba57c8bd6fe
rupika14 <85879889+rupika14@users.noreply.github.com>
2024-02-28 17:02:40 +0530

    Added Solution - GfG to GitHub

---
+++ School/Convert String to LowerCase/convert-string-to-lower-case.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def toLower (ob, S):
+        # code here 
+        return S.lower()
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        s = input()
+        
+        ob = Solution()
+        print(ob.toLower(s))
+# } Driver Code Ends
