904f77e578da989ac7e2e0e8021a0c62ad80806e
rupika14 <85879889+rupika14@users.noreply.github.com>
2024-02-28 18:58:24 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximum money/maximum-money.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def maximizeMoney(self, N , K):
+        # code here 
+        a=0
+        for i in range(0,N,2):
+            a+=K
+        return a
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N,K = map(int,input().split())
+        
+        ob = Solution()
+        print(ob.maximizeMoney(N,K))
+# } Driver Code Ends
